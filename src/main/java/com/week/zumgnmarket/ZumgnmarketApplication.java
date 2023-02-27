3391b87b1f7ea15a718998831e7faac9e9c800bb
kimueibin <at365@naver.com>
2023-02-27 23:12:44 +0900

    setting: Project Setting

---
+++ src/main/java/com/week/zumgnmarket/ZumgnmarketApplication.java
@@ -0,0 +1,13 @@
+package com.week.zumgnmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZumgnmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZumgnmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/week/zumgnmarket/ZumgnmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week.zumgnmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZumgnmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
