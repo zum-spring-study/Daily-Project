1cfbfec92db280cc6662df49b6a67b7e2a770b32
kimueibin <at365@naver.com>
2023-03-01 23:01:58 +0900

    feat : Swagger + QueryDSL Factory 세팅 추가

--- src/main/java/com/week/zumgnmarket/ZumgnmarketApplication.java
+++ src/main/java/com/week/zumgnmarket/ZumgnmarketApplication.java
@@ -2,8 +2,10 @@ package com.week.zumgnmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ZumgnmarketApplication {
 
     public static void main(String[] args) {
