10017cbeae0ba164a3e311fc7b446e8c0e86a854
kimueibin <at365@naver.com>
2023-03-02 01:53:45 +0900

    feat : Sell Facade 추가

--- src/main/java/com/week/zumgnmarket/endpoint/trade/facade/SellFacade.java
+++ src/main/java/com/week/zumgnmarket/endpoint/trade/facade/SellFacade.java
@@ -2,11 +2,23 @@ package com.week.zumgnmarket.endpoint.trade.facade;
 
 import com.week.zumgnmarket.endpoint.trade.dto.sell.TradeSellRequest;
 import com.week.zumgnmarket.endpoint.trade.dto.sell.TradeSellResponse;
+import com.week.zumgnmarket.service.trade.seller.SellProcessor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SellFacade {
+
+    private final SellProcessor sellProcessor;
     public TradeSellResponse sell(Long productId, TradeSellRequest tradeSellRequest) {
-        return null;
+
+        try{
+            sellProcessor.process(productId, tradeSellRequest);
+            return TradeSellResponse.of(productId, tradeSellRequest.getTradeStatus(), tradeSellRequest.getDescription());
+
+        }catch (Exception e){
+            return TradeSellResponse.of(productId, tradeSellRequest.getTradeStatus(), e.getMessage());
+        }
     }
 }
