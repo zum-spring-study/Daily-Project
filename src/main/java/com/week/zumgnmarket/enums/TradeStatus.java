ee2c22dabf85b7776ba94bc5fa6422bc1773cdb5
kimueibin <at365@naver.com>
2023-03-02 01:52:56 +0900

    chore : status 내 역제안 상태 추가

--- src/main/java/com/week/zumgnmarket/enums/TradeStatus.java
+++ src/main/java/com/week/zumgnmarket/enums/TradeStatus.java
@@ -2,5 +2,5 @@ package com.week.zumgnmarket.enums;
 
 public enum TradeStatus {
 
-    SELL, BUY, WAIT, SUGGEST, PROCESS, COMPLETE, CANCEL
+    SELL, BUY, WAIT, SUGGEST, RESUGGEST, COMPLETE, CANCEL
 }
